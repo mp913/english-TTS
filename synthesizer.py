dd6607bcd64c61a2e7bac79714449ea63e30310e
Max <zarkov01@mail.ru>
2022-05-17 15:32:11 +0300

    Move synthesis logic to separate class

---
+++ synthesizer.py
@@ -0,0 +1,95 @@
+import os
+import sys
+import torch
+import tps
+
+path_to_tacotron = "/home/max/TTS/pycharm-sova/sova-tts-engine"
+path_to_waveglow = "/home/max/TTS/pycharm-sova/waveglow"
+
+sys.path.insert(0, path_to_waveglow)
+MAX_WAV_VALUE = 32768.0  # value from mel2samp.py in waveglow
+
+sys.path.insert(0, path_to_tacotron)
+from hparams import create_hparams
+from model import load_model
+from utils.data_utils import TextMelLoader
+
+
+class Synthesizer:
+    def __init__(self, waveglow_path, is_fp16, sampling_rate, sigma, hparams_path, checkpoint_path, use_gst, emotion_wav, mask_stress,
+                 mask_phonemes):
+        # Waveglow initialization
+        self.waveglow = torch.load(waveglow_path)['model']
+        self.waveglow = self.waveglow.remove_weightnorm(self.waveglow)
+        self.waveglow.cuda().eval()
+
+        self.is_fp16 = is_fp16
+        self.sampling_rate = sampling_rate
+        self.sigma = sigma
+
+        if is_fp16:
+            from apex import amp
+            self.waveglow, _ = amp.initialize(self.waveglow, [], opt_level="O3")
+
+        print("Waveglow initialized")
+
+        # Tacotron2 initialization
+        assert os.path.isfile(hparams_path)
+        hparams = create_hparams(hparams_path)
+        hparams.path = hparams_path
+
+        self.model = load_model(hparams, False)
+
+        assert os.path.isfile(checkpoint_path)
+        print("Loading checkpoint '{}'".format(checkpoint_path))
+
+        checkpoint_dict = torch.load(checkpoint_path, map_location="cpu")
+        self.model.load_state_dict(checkpoint_dict["state_dict"])
+
+        self.model.cuda().eval()
+
+        self.charset = 'en'
+        self.text_handler = tps.Handler(self.charset)
+
+        self.use_gst = use_gst
+        if self.use_gst:
+            test_csv = "/home/max/TTS/pycharm-sova/inference_gst_test.csv"
+            self.text_loader = TextMelLoader(self.text_handler, test_csv, hparams)
+            self.ref_mel = torch.unsqueeze(self.text_loader.get_mel(emotion_wav), 0).cuda()
+
+        self.mask_stress = mask_stress
+        self.mask_phonemes = mask_phonemes
+
+        print("Tacotron2 initialized")
+
+    @staticmethod
+    def get_text(text, mask_stress, mask_phonemes, text_handler):
+        preprocessed_text = text_handler.process_text(
+            text, tps.cleaners.light_punctuation_cleaners, None, False,
+            mask_stress=mask_stress, mask_phonemes=mask_phonemes
+        )
+        preprocessed_text = text_handler.check_eos(preprocessed_text)
+        text_vector = text_handler.text2vec(preprocessed_text)
+
+        text_tensor = torch.IntTensor(text_vector)
+        return text_tensor
+
+    def synthesize(self, input_sentence, input_name):
+        processed_sentence = self.get_text(input_sentence, self.mask_stress, self.mask_phonemes, self.text_handler)
+        processed_sentence = torch.unsqueeze(processed_sentence, 0)
+        processed_sentence = processed_sentence.cuda()
+        if self.use_gst:
+            mel_outputs, mel_outputs_postnet, gates, alignments = self.model.inference(processed_sentence,
+                                                                                       reference_mel=self.ref_mel)
+        else:
+            mel_outputs, mel_outputs_postnet, gates, alignments = self.model.inference(processed_sentence)
+        mel = mel_outputs_postnet[0]
+        mel = torch.autograd.Variable(mel.cuda())
+        mel = torch.unsqueeze(mel, 0)
+        mel = mel.half() if self.is_fp16 else mel
+        audio = self.waveglow.infer(mel, sigma=self.sigma)
+        audio = audio * MAX_WAV_VALUE
+        audio = audio.squeeze()
+        audio = audio.cpu().numpy()
+        audio = audio.astype('int16')
+        return audio
--- tts.py
+++ tts.py
@@ -1,37 +1,11 @@
 import argparse
-import csv
-import librosa
-import numpy
 import os
 from scipy.io.wavfile import write
-import sys
 import torch
-import tps
 import yaml
 from yaml.loader import SafeLoader
 
-path_to_tacotron = "/home/max/TTS/pycharm-sova/sova-tts-engine"
-path_to_waveglow = "/home/max/TTS/pycharm-sova/waveglow"
-
-sys.path.insert(0, path_to_waveglow)
-MAX_WAV_VALUE = 32768.0  # value from mel2samp.py in waveglow
-
-sys.path.insert(0, path_to_tacotron)
-from hparams import create_hparams
-from model import load_model
-from utils.data_utils import TextMelLoader
-
-
-def get_text(text, mask_stress, mask_phonemes, text_handler):
-    preprocessed_text = text_handler.process_text(
-        text, tps.cleaners.light_punctuation_cleaners, None, False,
-        mask_stress=mask_stress, mask_phonemes=mask_phonemes
-    )
-    preprocessed_text = text_handler.check_eos(preprocessed_text)
-    text_vector = text_handler.text2vec(preprocessed_text)
-
-    text_tensor = torch.IntTensor(text_vector)
-    return text_tensor
+import synthesizer
 
 
 def get_text_and_names_from_csv(csv_path):
@@ -54,12 +28,15 @@ if __name__ == "__main__":
                         required=False, help="Name of the result .wav file")
     parser.add_argument("-c", "--csv_to_speech", type=str, default="",
                         required=False, help="Path to .csv file with text for synthesis")
+    parser.add_argument("-s", "--use_ssml", action='store_true', default=False,
+                        required=False, help="Enable SSML parsing")
     args = parser.parse_args()
 
     config_path = args.config_path
     console_text = args.text_to_speech
     console_name = args.file_name
     csv_path = args.csv_to_speech
+    use_ssml = args.use_ssml
 
     names = []
     texts = []
@@ -83,77 +60,28 @@ if __name__ == "__main__":
 
         waveglow_path = data['waveglow_path']
         is_fp16 = data['is_fp16']
-        #denoiser_strength = data['denoiser_strength']
         sigma = data['sigma']
         sampling_rate = data['sampling_rate']
 
         print("Config file loaded successfully")
 
-    # Waveglow initialization
-    waveglow = torch.load(waveglow_path)['model']
-    waveglow = waveglow.remove_weightnorm(waveglow)
-    waveglow.cuda().eval()
-
-    if is_fp16:
-        from apex import amp
-        waveglow, _ = amp.initialize(waveglow, [], opt_level="O3")
-
-    print("Waveglow initialized")
-
-    # Tacotron2 initialization
-    assert os.path.isfile(hparams_path)
-    hparams = create_hparams(hparams_path)
-    hparams.path = hparams_path
-
-    model = load_model(hparams, False)
-
-    assert os.path.isfile(checkpoint_path)
-    print("Loading checkpoint '{}'".format(checkpoint_path))
-
-    checkpoint_dict = torch.load(checkpoint_path, map_location="cpu")
-    model.load_state_dict(checkpoint_dict["state_dict"])
-
-    model.cuda().eval()
-
-    charset = 'en'
-    text_handler = tps.Handler(charset)
-
-    if use_gst:
-        test_csv = "/home/max/TTS/pycharm-sova/inference_gst_test.csv"
-        text_loader = TextMelLoader(text_handler, test_csv, hparams)
-        ref_mel = torch.unsqueeze(text_loader.get_mel(emotion_wav), 0).cuda()
-
     mask_stress = False
     mask_phonemes = True
 
-    print("Tacotron2 initialized")
-
+    synthesizer = synthesizer.Synthesizer(waveglow_path=waveglow_path,
+                                          is_fp16=is_fp16,
+                                          sampling_rate=sampling_rate,
+                                          sigma=sigma,
+                                          hparams_path=hparams_path,
+                                          checkpoint_path=checkpoint_path,
+                                          use_gst=use_gst,
+                                          emotion_wav=emotion_wav,
+                                          mask_stress=mask_stress,
+                                          mask_phonemes=mask_phonemes)
     # Work cycle
     with torch.no_grad():
         for input_sentence, input_name in zip(texts, names):
-            processed_sentence = get_text(input_sentence, mask_stress, mask_phonemes, text_handler)
-            processed_sentence = torch.unsqueeze(processed_sentence, 0)
-            processed_sentence = processed_sentence.cuda()
-            if use_gst:
-                mel_outputs, mel_outputs_postnet, gates, alignments = model.inference(processed_sentence,
-                                                                                      reference_mel=ref_mel)
-            else:
-                mel_outputs, mel_outputs_postnet, gates, alignments = model.inference(processed_sentence)
-            #mel_save_path = os.path.join(output_folder, f'{input_name}.pt')
-            #torch.save(mel_outputs_postnet[0], mel_save_path)
-
-            #file_name = os.path.splitext(os.path.basename(mel_save_path))[0]
-            #mel = torch.load(mel_save_path)
-            mel = mel_outputs_postnet[0]
-            mel = torch.autograd.Variable(mel.cuda())
-            mel = torch.unsqueeze(mel, 0)
-            mel = mel.half() if is_fp16 else mel
-            audio = waveglow.infer(mel, sigma=sigma)
-            audio = audio * MAX_WAV_VALUE
-            audio = audio.squeeze()
-            audio = audio.cpu().numpy()
-            audio = audio.astype('int16')
-            audio_path = os.path.join(
-                output_folder, "{}".format(input_name))
+            audio = synthesizer.synthesize(input_sentence, input_name)
+            audio_path = os.path.join(output_folder, "{}".format(input_name))
             write(audio_path, sampling_rate, audio)
             print(audio_path)
